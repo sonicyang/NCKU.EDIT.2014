12be0890d8a3ca3609ad4aaa0abd5845f2d4a948
KuoE0 <kuoe0.tw@gmail.com>
2014-01-18 04:35:12 +0800

    add self js file

---
+++ static/js/main.js
@@ -0,0 +1,14 @@
+/*
+ * main.js
+ * Copyright (C) 2014 KuoE0 <kuoe0.tw@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+
+$(function () {
+	// initial Semantic UI modules
+	$('.dropdown').dropdown();
+
+
+});
