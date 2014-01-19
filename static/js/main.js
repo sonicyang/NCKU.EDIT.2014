0f6b093abbdc40299f7f0b567b8bfd57e14cc263
KuoE0 <kuoe0.tw@gmail.com>
2014-01-20 03:43:36 +0800

    responsive design for section title

--- static/js/main.js
+++ static/js/main.js
@@ -10,5 +10,34 @@ $(function () {
 	// initial Semantic UI modules
 	$('.dropdown').dropdown();
 
+	$(window).resize(function () {
+
+		var target = $('div[id$="-content"] h1');
+
+		target.each(function () {
+			var w = $(window).width();
+
+			$(this).removeClass('huge');
+			$(this).removeClass('large');
+			$(this).removeClass('medium');
+			$(this).removeClass('small');
+
+			if (w >= 980) {
+				$(this).addClass('huge');
+			}
+			else if (w >= 768 && w < 980) {
+				$(this).addClass('large');
+			}
+			else if (w > 480 && w < 768) {
+				$(this).addClass('medium');
+			}
+			else {
+				$(this).addClass('small');
+			}
+
+		});
+
+
+	});
 
 });
